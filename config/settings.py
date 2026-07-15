ae8a4774910dc9a2562c54e7491aab3c83ed0785
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-07-15 20:57:23 +0330

    Add 'django.contrib.admin', 'django.contrib.sessions','django.contrib.messages', in installed apps.

--- config/settings.py
+++ config/settings.py
@@ -30,8 +30,11 @@ ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
+    'django.contrib.admin', 
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions', 
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'users'
 ]

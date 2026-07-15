9e59b9f4ff0eb8b3db6e0eaeb1d537109082a560
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-07-15 17:24:07 +0330

    Stores the time of the last activity or session creation.

--- users/models.py
+++ users/models.py
@@ -5,8 +5,9 @@ class SessionStorage(models.Model):
 
     session_key = models.CharField(max_length=255, unique=True)
     
-   
     session_data = models.TextField()
+
+    updated_at = models.DateTimeField(auto_now=True) 
 
     def __str__(self):
         return f"Session: {self.session_key}"

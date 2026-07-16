a8a8b97f98717075a3e8ccff45923fa6e1802c9e
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-07-16 14:53:40 +0330

    Add class SessionStore

--- users/middleware.py
+++ users/middleware.py
@@ -7,3 +7,20 @@ from users.models import SessionStorage
 COOKIE_NAME = 'my_custom_session_id'
 SESSION_AGE = timedelta(minutes=30)
 
+class SessionStore(dict):
+    """
+    A dictionary that has the necessary methods to be compatible with django.contrib.auth.
+    """
+
+    def __init__(self, session_key=None):
+        super().__init__()
+        self.session_key = session_key
+        self.modified = False
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        self.modified = True
+
+    def __delitem__(self, key):
+        super().__delitem__(key)
+        self.modified = True

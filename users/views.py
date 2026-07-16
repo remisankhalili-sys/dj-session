1c4045b0d47f2ac86a536802f00d00b0436be31a
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-07-16 15:32:46 +0330

    Add  import authenticate, login, logout

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views import View
 from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, login, logout
 
 User = get_user_model()
 

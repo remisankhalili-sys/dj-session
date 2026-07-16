0da651d4dbd869f16fb8cb4376c18eae70797a2b
remisankhalili-sys <remisa.nkhalili@gmail.com>
2026-07-16 15:50:34 +0330

    Add LogoutView in urls

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from users.views import RegisterView, LoginView, ProfileView, SessionStatusView, SessionTestView
+from users.views import RegisterView, LoginView, LogoutView, ProfileView, SessionStatusView, SessionTestView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('session-status/', SessionStatusView.as_view(), name='session_status'),
     path('session-test/', SessionTestView.as_view(), name='session_test'),
